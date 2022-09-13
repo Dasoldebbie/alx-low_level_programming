03608142465e66d8fb7f1d954966c0cb3ab85f1d
Dasoldebbie <osinbowaleoluwadara@gmail.com>
2022-09-12 20:02:35 -0700

    ‘hexa’

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,13 +4,14 @@
  * main - Entry point
  *
  * Return: 0
- */int main(void)
+ */
+int main(void)
 {
 	int i;
 
 	for (i = 48; i < 58; i++)
 		putchar(i);
-	for (i = 'a'; i = 'f'; i++)
+	for (i = 'a'; i <= 'f'; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
